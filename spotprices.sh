40e71fdd5d0a8c333b96ec0f3b2290bacea43c26
zqiu <zqiu1994@gmail.com>
2023-04-03 17:59:36 -0400

    updated instances checked and wrote to file

--- spotprices.sh
+++ spotprices.sh
@@ -1,6 +1,7 @@
 REGIONS=("us-east-1" "us-east-2")
-INSTANCES="c6a.4xlarge c6g.4xlarge c6i.4xlarge c7g.4xlarge"
+INSTANCES="c6a.4xlarge c6i.4xlarge c6id.4xlarge c6in.4xlarge c7g.4xlarge"
 
 for i in ${REGIONS[@]}; do
-	aws --region=${REGIONS[$i]} ec2 describe-spot-price-history --instance-types $INSTANCES --start-time=$(date +%s) --product-descriptions="Linux/UNIX"  --query 'SpotPriceHistory[*].{az:AvailabilityZone, price:SpotPrice, type:InstanceType}'
+	aws --region=${REGIONS[$i]} ec2 describe-spot-price-history --instance-types $INSTANCES --start-time=$(date +%s) --product-descriptions="Linux/UNIX"  --query 'SpotPriceHistory[*].{az:AvailabilityZone, price:SpotPrice, type:InstanceType}' >> 4xlarge
 done
+
